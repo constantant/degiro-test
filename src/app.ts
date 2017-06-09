c872ecceef28830d8853f72c62a1ac6bf2e7db23
constantant <constantant@mail.ru>
2017-06-09 19:28:55 +0300

    task-1

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 export function sortProducts(products: ProductItem[], options?: Options): Result {
-    let size = (options.size || 5),
-        sortedList = products.sort(
-            (product1: ProductItem, product2: ProductItem) => product1.price - product2.price
-        );
+    let size = typeof options.size === 'number' ? options.size : 5,
+        productsLength = products.length;
+
+    if (size < 1 || size > productsLength) {
+        return {
+            highest: null,
+            lowest: null
+        }
+    }
+
+    let highestSize = size,
+        lowestSize = productsLength < size * 2 ? productsLength - size : size;
+
+    let sortedList = products.sort(
+        (product1: ProductItem, product2: ProductItem) => product1.price - product2.price
+    );
 
     return {
-        highest: sortedList.splice(sortedList.length - size - 1, size),
-        lowest: sortedList.splice(0, size),
+        highest: sortedList.splice(productsLength - highestSize - 1, highestSize),
+        lowest: lowestSize < 1 ? null : sortedList.splice(0, lowestSize)
     }
 }
